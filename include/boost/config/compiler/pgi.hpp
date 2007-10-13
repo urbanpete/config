b842a77f769cfb2e5961bb5f389a5a48f62af280
Daniel James <daniel@calamity.org.uk>
2007-10-13 23:18:35 +0000

    Merge with the offending files removed.
    [SVN r39995]

--- include/boost/config/compiler/pgi.hpp
+++ include/boost/config/compiler/pgi.hpp
@@ -1,28 +1,25 @@
-//  (C) Copyright John Maddock 2001 - 2002. 
+//  (C) Copyright Noel Belcourt 2007.
 //  Use, modification and distribution are subject to the 
 //  Boost Software License, Version 1.0. (See accompanying file 
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 //  See http://www.boost.org for most recent version.
 
-//  SGI C++ compiler setup:
+//  PGI C++ compiler setup:
 
-#define BOOST_COMPILER "SGI Irix compiler version " BOOST_STRINGIZE(_COMPILER_VERSION)
-
-#include "boost/config/compiler/common_edg.hpp"
+#define BOOST_COMPILER "PGI compiler version " BOOST_STRINGIZE(_COMPILER_VERSION)
 
 //
 // Threading support:
 // Turn this on unconditionally here, it will get turned off again later
 // if no threading API is detected.
 //
-#define BOOST_HAS_THREADS
-#define BOOST_NO_TWO_PHASE_NAME_LOOKUP
 
-#undef BOOST_NO_SWPRINTF
-#undef BOOST_DEDUCED_TYPENAME
+#define BOOST_FUNCTION_SCOPE_USING_DECLARATION_BREAKS_ADL 
+#define BOOST_NO_TWO_PHASE_NAME_LOOKUP
+#define BOOST_NO_SWPRINTF
+
 //
 // version check:
 // probably nothing to do here?
-
 
